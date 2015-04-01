8580b4cb27aa06e506ef4ac398ff500fa2e93edb
Dmytro Sirko <dimasirko@gmail.com>
2015-04-01 09:08:11 +0300

    Store controller added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+    validates :tittle, :description, :image_url, presence: true
+    validates :tittle, length: {minimum: 6}
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :tittle, uniqueness: true
+    validates :image_url, allow_blank: true, format: {
+                            with: %r{\.(gif|jpg|png)\Z}i,
+                            message: 'must be a URL for GIF, JPG or PNG image.'
+                            # URL ?????? ????????? ?? ??????????? ??????? GIF, JPG ??? PNG
+                        }
 end
