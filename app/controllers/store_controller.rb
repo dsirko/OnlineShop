85063c19dcff14b3cad577edc89acd2b78fa4bbd
Dmytro Sirko <dimasirko@gmail.com>
2015-04-03 14:38:21 +0300

    Cart upgrated

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  attr_accessor :count
+
   def index
+    increment_counter
     @products = Product.order(:tittle)
+  end
+
+  def increment_counter
+    session[:counter] ||= 0
+    @count = session[:counter] += 1
+    @count >= 5 ? @shown_message = "You've been here #{@count} times" : nil
   end
 end
