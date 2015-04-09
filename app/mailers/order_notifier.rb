34b2bbbdf99f2fca97598b5c68b9176759774650
Dmytro Sirko <dimasirko@gmail.com>
2015-04-09 14:51:51 +0300

    Added mailer for orders

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,25 @@
+class OrderNotifier < ApplicationMailer
+  default from: 'Sam Ruby <shop@example.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: '????????????? ?????? ? OnlineStore'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped
+    @order = order
+
+    mail to: order.email, subject: 'Order from OnlineStore has been sent'
+  end
+end
